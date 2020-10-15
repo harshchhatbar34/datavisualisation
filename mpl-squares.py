0c4a6c26f3f84d61eef0a7a7760448ce852c5a10
harshchhatbar34 <harshchhatbar34@gmail.com>
2020-10-15 12:29:48 +0530

    added input value

--- mpl-squares.py
+++ mpl-squares.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
-squares = [1, 4, 9, 16]
-plt.plot(squares,linewidth=5)
+input_number = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_number, squares,linewidth=5)
 plt.title("square Numbers", fontsize=24)
 plt.xlabel("value", fontsize=14)
 plt.ylabel("Square of value", fontsize=14)
