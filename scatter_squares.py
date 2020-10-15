f86eedf9137051f4840f9e7e6633650daa36c1a9
harshchhatbar34 <harshchhatbar34@gmail.com>
2020-10-15 18:24:39 +0530

    cube of first 5000 numericals

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 
-x_value = [1, 2, 3, 4, 5]
-y_value = [1, 4, 9, 16, 25]
-plt.scatter(x_value, y_value, s=200, c='black')
+x_value = list(range(1,1001))
+y_value = [x**2 for x in x_value]
+plt.scatter(x_value, y_value, c=y_value, cmap=plt.cm.Reds, edgecolor='none', s=40)
 plt.title("Square Number", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("square of value", fontsize=14)
 
 #Set size of tick labels.
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+#set the range of each axis
+plt.axis([0,1100,0,1100000])
 plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
